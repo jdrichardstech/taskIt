400a3608e51bd203a7cb69e53ab330c369be64ca
jdrichardstech <jdrichardstech@gmail.com>
2017-01-17 14:05:34 -0500

    update twilio route

--- routes/twilio.js
+++ routes/twilio.js
@@ -7,7 +7,21 @@ router.post('/task', function(req, res, next) {
 
 
   console.log("TWILIO" + JSON.stringify(req.body))
-    // res.render('home', { title: 'Express' })
+  var message = req.body['Body']
+  var task = {
+    title:'Twilio Task',
+    category:'delivery',
+    description: message
+  }
+  controllers.task.post(task, false)
+  .then(function(result){
+    console.log("Success: "+ JSON.stringify(result))
+    res.send("Hello")
+  })
+  .catch(function(err){
+    console.log("Error: " + err)
+  })
+
 });
 
 
