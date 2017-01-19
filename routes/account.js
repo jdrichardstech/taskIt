7dc326c85fa3075d97628db98b5f89db1609429d
jdrichardstech <jdrichardstech@gmail.com>
2017-01-18 20:22:44 -0500

    reset session

--- routes/account.js
+++ routes/account.js
@@ -27,9 +27,10 @@ router.get('/:action', function(req, res, next) {
 
      jwt.verify(req.session.token, process.env.TOKEN_SECRET, function(err, decoded){
        if(err){
+         req.session.reset()
          res.json({
-           confirmation: 'fail',
-           message: 'Access Denied'
+           confirmation: 'success',
+           message: 'null'
          })
          return
        }
