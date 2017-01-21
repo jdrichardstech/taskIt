e3f2109a741bd7d4d97420ee757a66ed5ebcbb32
jdrichardstech <jdrichardstech@gmail.com>
2017-01-21 14:56:48 -0500

    debut

--- utils/TwilioHelper.js
+++ utils/TwilioHelper.js
@@ -8,7 +8,7 @@ module.exports = {
       if(recipient.indexOf('+1')==-1)
       recipient = '+1'+recipient
 
-      var client = new twilio.RestClient('ACbef92078becdc5aa7f2fb3fde9bd7968', '0c2e4c7bbf814ef1bf7d00b1285ce4c0';
+      var client = new twilio.RestClient('ACbef92078becdc5aa7f2fb3fde9bd7968', '0c2e4c7bbf814ef1bf7d00b1285ce4c0')
 
 
       client.messages.create({
